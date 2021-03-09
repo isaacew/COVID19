4da0e86dd6bc9a4f27e9c95674ecc132e3af8002
isaacew <isaacweintraub@gmail.com>
2021-03-09 08:30:41 -0500

    added todays state data

--- automator.m
+++ automator.m
@@ -29,6 +29,9 @@ cd('/Users/weintraub0/Documents/Weintraub/Projects/COVID19')
 % else
 tic
 statePlot = 1;
+
+
+
 % Import the data from CSV format to matlab cell or matrix 
 pops      = importdata("populationData/data/population.csv");
 cases     = importdata("COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
